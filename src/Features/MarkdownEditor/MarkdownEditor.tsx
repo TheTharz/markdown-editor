52807c40088bd453015d9cf880ef7d0583530b70
Tharindu Imalka <tharinduimalka915@gmail.com>
2024-02-06 11:14:53 +0530

    Add MarkdownEditor CSS and update MarkdownEditor component

--- src/Features/MarkdownEditor/MarkdownEditor.tsx
+++ src/Features/MarkdownEditor/MarkdownEditor.tsx
@@ -5,6 +5,7 @@ import remarkGfm from 'remark-gfm';
 import rehypeRaw from 'rehype-raw';
 import Markdown from 'react-markdown';
 import CopyToClipboard from 'react-copy-to-clipboard';
+import './MarkdownEditor.css';
 
 const MarkdownEditor: React.FC = () => {
   const defaultMarkdown = '## Markdown preview';
@@ -29,8 +30,8 @@ const MarkdownEditor: React.FC = () => {
   };
 
   return (
-    <div className='flex h-screen bg-slate-900 text-white'>
-      <div className='flex-1 p-4'>
+    <div className='flex-container'>
+      <div className='textarea-container'>
         <textarea
           value={markdownText}
           onChange={handleMarkdownChange}
@@ -38,19 +39,16 @@ const MarkdownEditor: React.FC = () => {
           className='w-full h-full border p-2 rounded overflow-auto text-slate-900'
         />
       </div>
-      <div className='flex-1 border p-4 overflow-auto'>
-        <div className='flex justify-end mb-4'>
+      <div className='preview-container'>
+        <div className='button-container'>
           <CopyToClipboard text={markdownText} onCopy={() => setIsCopied(true)}>
-            <button className='bg-blue-500 text-white px-4 py-2 rounded mr-2'>
+            <button className='copy-button'>
               <FontAwesomeIcon icon={faCopy} />
 
               {isCopied ? <span className='ml-2'>Copied!</span> : null}
             </button>
           </CopyToClipboard>
-          <button
-            onClick={downloadMarkdownFile}
-            className='bg-slate-500 text-white px-4 py-2 rounded'
-          >
+          <button onClick={downloadMarkdownFile} className='download-button'>
             <FontAwesomeIcon icon={faDownload} />
           </button>
         </div>
